f3e7b28f4cdb582a892fcee123f5609f13b03d0e
Gevin <gevinpinto@gmail.com>
2023-08-16 11:23:17 +0530

    initial commit

---
+++ python_progs/arith.py
@@ -0,0 +1,6 @@
+def add(n1,n2):
+    return(n1+n2)
+
+a=int(input("Enter a num1:"))
+b=int(input("Enter a num2:"))
+print("sum=",add(a,b))
--- readme.txt
+++ readme.txt
@@ -1,2 +1,1 @@
-sams denieal
-banglore jayram cross
+Python program
