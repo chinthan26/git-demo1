a7429c7fb6f0469cac213b0d68a7562460548a7a
Gevin <gevinpinto@gmail.com>
2023-08-16 12:32:26 +0530

    Added div function to arith.py file

--- python_progs/arith.py
+++ python_progs/arith.py
@@ -7,9 +7,13 @@ def sub(n1,n2):
 def mul(n1,n2):
     return(n1*n2)
 
+def div(n1,n2):
+    return(n1/n2)
+
 
 a=int(input("Enter a num1:"))
 b=int(input("Enter a num2:"))
 print("sum=",add(a,b))
 print("sub=",sub(a,b))
 print("mul=",mul(a,b))
+print("div=",div(a,b))
