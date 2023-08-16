72774acdc2752bbf82e6b2ea9a3388eaa2de84fc
charancs87 <charancs5660@gmail.com>
2023-08-16 15:46:34 +0530

    root added

--- python_progs/arith.py
+++ python_progs/arith.py
@@ -9,7 +9,8 @@ def mul(n1,n2):
 
 def div(n1,n2):
     return(n1/n2)
-
+def root(n1):
+    return(n1*n1)
 
 a=int(input("Enter a num1:"))
 b=int(input("Enter a num2:"))
@@ -17,3 +18,5 @@ print("sum=",add(a,b))
 print("sub=",sub(a,b))
 print("mul=",mul(a,b))
 print("div=",div(a,b))
+n=int(input("Enter a num1:"))
+print(root(n))
